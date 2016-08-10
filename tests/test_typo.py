b4d03ad40b068d36b06146737ad20a11053d8ccf
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-08-10 20:41:56 +0100

    Check for string representations of handlers

--- tests/test_typo.py
+++ tests/test_typo.py
@@ -10,6 +10,7 @@ from typo.handlers import Handler, AnyHandler, TypeHandler
 def test_any():
     h = Handler(Any)
     assert isinstance(h, AnyHandler)
+    assert str(h) == 'Any'
     c = h.compile()
     c(None)
     c([1, '2', {}])
@@ -18,6 +19,7 @@ def test_any():
 def test_builtin_type():
     h = Handler(int)
     assert isinstance(h, TypeHandler)
+    assert str(h) == 'int'
     c = h.compile()
     c(1)
     pytest.raises_regexp(TypeError, 'expected int, got str', c, 'foo')
@@ -27,6 +29,7 @@ def test_user_type():
     A = type('A', (), {})
     h = Handler(A)
     assert isinstance(h, TypeHandler)
+    assert str(h) == 'test_typo.A'
     c = h.compile()
     c(A())
     pytest.raises_regexp(TypeError, 'expected test_typo.A, got int', c, 1)
