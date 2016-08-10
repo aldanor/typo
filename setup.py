f461b66cfc49dc86dcc3d8289cb67cd47053a191
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-08-10 20:10:03 +0100

    Add setup.py and _version.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import io
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = ''
+with io.open('typo/_version.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='typo',
+    version=version,
+    description='Runtime type checking for functions with type annotations',
+    author='Ivan Smirnov',
+    author_email='i.s.smirnov@gmail.com',
+    url='https://github.com/aldanor/typo',
+    license='MIT',
+    packages=['typo'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ),
+    keywords='typing type checking annotations'
+)
