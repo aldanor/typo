95a176aaef45338f71afa38a9c6f615b953c9b2f
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-11-05 19:45:58 +0000

    Add a basic test for @type_check

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -8,3 +8,14 @@ def test_wrapper(x: int, *args, **kwargs: type('T', (), {})) -> int:
     "Test docstring."
 
 
+@type_check_test(
+    ok=[
+        _(1.1)
+    ],
+    fail=[
+        (_(1), 'invalid `x`: expected float, got int'),
+        (_('foo'), 'invalid `x`: expected float, got str'),
+        (_(0.), 'invalid return value: expected tuple, got float')
+    ])
+def test_foo(x: float) -> tuple:
+    return (x, x) if x else x
