c3907bdc833ee480182d6acce45812e2dbc566be
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-11-05 23:17:17 +0000

    Add proper support for keyword args and varargs

--- typo/decorator.py
+++ typo/decorator.py
@@ -3,18 +3,67 @@
 import inspect
 import functools
 
-from inspect import _ParameterKind as ParamKind
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 
 from typo.codegen import Codegen
 from typo.handlers import Handler
 
 
+class KeywordArgsHandler(Handler):
+    def __init__(self, bound: Any) -> None:
+        super().__init__(bound)
+        self.handler = Handler(bound)
+
+    def __call__(self, gen: Codegen, varname: str, desc: Optional[str]) -> None:
+        if not self.handler.is_any:
+            var_k, var_v = gen.new_vars(2)
+            gen.write_line('for {}, {}, in {}.items():'.format(var_k, var_v, varname))
+            with gen.indent():
+                self.handler(gen, var_v, None if desc is None else
+                             'keyword argument `{{{}}}`'.format(var_k))
+
+    def __str__(self) -> str:
+        return 'KeywordArgs[{}]'.format(self.handler)
+
+
+class PositionalArgsHandler(Handler):
+    def __init__(self, bound: Any) -> None:
+        super().__init__(bound)
+        self.handler = Handler(bound)
+
+    def __call__(self, gen: Codegen, varname: str, desc: Optional[str]) -> None:
+        if not self.handler.is_any:
+            gen.enumerate_and_check(varname, desc, self.handler)
+
+    def __str__(self) -> str:
+        return 'PositionalArgs[{}]'.format(self.handler)
+
+
 def type_check(func: Callable) -> Callable:
-    # Create handlers for all annotated arguments and the return value.
     annotations = func.__annotations__
-    handlers = {k: Handler(v) for k, v in annotations.items() if k != 'return'}
+
+    # Extract function signature without type annotations -- this is because annotations
+    # may contain user types, so we don't want to stringify them and instead pass the
+    # annotations dict to the wrapped function as is.
+    func.__annotations__ = {}
+    signature = inspect.signature(func)
+    func.__annotations__ = annotations
+
+    # Build call arguments and type checking handlers for annotated arguments.
     return_handler = Handler(annotations.get('return', Any))
+    call_args, handlers = [], {}
+    for arg, param in signature.parameters.items():
+        handler_type = Handler
+        call_prefix = arg + '='
+        if param.kind == inspect._VAR_KEYWORD:
+            handler_type = KeywordArgsHandler
+            call_prefix = '**'
+        elif param.kind == inspect._VAR_POSITIONAL:
+            handler_type = PositionalArgsHandler
+            call_prefix = '*'
+        call_args.append(call_prefix + arg)
+        if arg in annotations:
+            handlers[arg] = handler_type(annotations[arg])
 
     # Generate a set of all typevars used in the function signature.
     typevars = set.union(return_handler.typevars, *(h.typevars for h in handlers.values()))
@@ -24,22 +73,17 @@ def type_check(func: Callable) -> Callable:
     return_var, func_var = gen.new_vars(2)
     gen.context[func_var] = func
 
-    # Extract function signature without type annotations -- this is because annotations
-    # may contain user types, so we don't want to stringify them and instead pass the
-    # annotations dict to the wrapped function as is.
-    func.__annotations__ = {}
-    signature = inspect.signature(func)
-    func.__annotations__ = annotations
-
-    # TODO: proper handling of varargs / kwargs, like in mypy
+    # Generate code for the function body.
     gen.write_line('def {}{}:'.format(func.__name__, str(signature)))
     with gen.indent():
-        call_args = []
-        arg_prefix = {ParamKind.POSITIONAL_ONLY: '*', ParamKind.VAR_KEYWORD: '**'}
-        for arg, param in signature.parameters.items():
+        for arg in signature.parameters:
             if arg in handlers:
-                handlers[arg](gen, arg, '`{}`'.format(arg))
-            call_args.append(arg_prefix.get(param.kind, arg + '=') + arg)
+                handler = handlers[arg]
+                var_desc = {
+                    KeywordArgsHandler: 'keyword arguments',
+                    PositionalArgsHandler: 'positional arguments'
+                }.get(type(handler), '`{}`'.format(arg))
+                handler(gen, arg, var_desc)
         gen.write_line('{} = {}({})'.format(return_var, func_var, ', '.join(call_args)))
         return_handler(gen, return_var, 'return value')
         gen.write_line('return {}'.format(return_var))
